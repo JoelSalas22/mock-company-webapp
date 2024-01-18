9c1e405ba73f9fce2223f980c459dfc059837829
Joel Salas <joel.salas.093@gmail.com>
2024-01-18 14:42:43 -0500

    added Jenkinsfile.java

---
+++ Jenkinsfile.java
@@ -0,0 +1,28 @@
+pipeline {
+  /*
+   * TODO: Implement pipeline stages/steps
+   *   See documentation: https://www.jenkins.io/doc/book/pipeline/syntax/#stages
+   */
+   agent any
+
+    stages {
+    stage('Build') {
+    steps {
+        echo 'Building..'
+        Build: sh './gradlew build'
+        }
+    }
+    stage('Test') {
+    steps {
+        echo 'Testing..'
+        Test: sh './gradlew test'
+        }
+    }
+//     stage('Deploy') {
+//       steps {
+//         echo 'Deploying....'
+//       }
+//     }
+
+}
+}
